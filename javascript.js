bbf538ab1a3f35490fc9e7ca2913f515a769d072
matiasmoralesolivos-lab <matiasmoralesolivos@gmail.com>
2026-02-23 21:25:01 -0300

    mensajillo

--- javascript.js
+++ javascript.js
@@ -2,3 +2,8 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+
+edad = prompt ("introduce tu edad")
+if (edad < 18) { alert ("No puedes comprar el producto") } 
+else if (edad >= 18) { alert ("eres una puerquita") }
